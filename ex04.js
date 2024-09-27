c553d653bc7fc515a18e0f6480dbc3ab166eb09e
Julio-SC <jhver2001@gmail.com>
2024-09-27 12:09:37 -0700

    ex04 completed

--- ex04.js
+++ ex04.js
@@ -3,7 +3,10 @@
 // Test your function by calculating the sum of the array and logging the result to the console.
 
 function sumOfDigits(n) {
-  // Your code here
-}
+  
+  return n.reduce((storage, current) => storage + current, 0);
+
+  } 
+
 
 console.log(sumOfDigits([1, 3, 7, 1, 8])); // 20
